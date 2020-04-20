5b72e591821cf71c8f34b026392ca388cfd83bee
22SkiTTleS22 <iliy220599@mail.ru>
2020-04-20 17:13:15 +0400

    LR1-fix-4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,18 +13,16 @@ public class Main {
         char[] strArray = str.toCharArray();
         int maxLength = 1;
         ArrayList<Character> mas = new ArrayList<Character>();
-        mas.add(strArray[0]);
-        for (int j = 1; j <= str.length()-1; j++) {
+        for (int j = 0; j < str.length(); j++) {
             for (int i = j; i < str.length(); i++) {
                 if (mas.contains(strArray[i])) {
                     if (maxLength < mas.size()) {
                         maxLength = mas.size();
-                        mas.clear();
                         mas.add(strArray[i]);
                     }
+                    mas.clear();
                 } else {
                     mas.add(strArray[i]);
-                    maxLength = mas.size();
                 }
             }
         }
