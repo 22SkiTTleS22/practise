2eed17c58c798dfe595c0dc417ba0a13be815e30
22SkiTTleS22 <iliy220599@mail.ru>
2020-04-20 16:00:13 +0400

    LR1-fix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,26 +1,30 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-
+    //    Условие: дана строка, ввод с клавиатуры. Найти в ней длину максимальной
+//    подстроки из неповторяющихся символов.
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Input string: ");
         String str = in.nextLine();
         char[] strArray = str.toCharArray();
-        char ch = strArray[0];
-        int count = 1;
         int maxLength = 1;
-        for (int i = 1; i < str.length(); i++) {
-            if (ch == strArray[i]) {
-                count++;
-            } else  {
-                count = 1;
-            }
-            ch = strArray[i];
-            if(count > maxLength) {
-                maxLength = count;
+        ArrayList<Character> mas = new ArrayList<Character>();
+        mas.add(strArray[0]);
+        for (int j = 1; j < str.length()-1; j++) {
+            for (int i = j; i < str.length(); i++) {
+                if (mas.contains(strArray[i])) {
+                    if (maxLength < mas.size()) {
+                        maxLength = mas.size();
+                        mas.clear();
+                        mas.add(strArray[i]);
+                    }
+                } else {
+                    mas.add(strArray[i]);
+                }
             }
         }
         System.out.println(maxLength);
